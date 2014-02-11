3bfc85dc3613302b45d7593d629f8323c43da9f6
Ryan Young <ry525525@gmail.com>
2014-02-11 10:55:18 -0500

    Updated install

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [[ `echo "$0" | grep "/" | wc -l` > 0 ]]; then
+    cd ${0%/*}
+fi
 cd ..
 PATH_TO_PACKAGE="$(pwd)"
 NEW_ROS_LINE="export ROS_PACKAGE_PATH=\$ROS_PACKAGE_PATH:"$PATH_TO_PACKAGE
