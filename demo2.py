1aeea627d5c240ea7ba60695fdc82e287cffba49
John Maloney <jmal0320@gmail.com>
2014-02-11 10:42:02 -0500

    Move methods

---
+++ demo2.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+'''
+This node is run in conjunction with the biotac_sensors exceutable. It will
+get the pressure from the biotac sensor and correlate it with elbow joint commands
+on hubo.
+
+Author: Ryan
+'''
+
+import roslib; roslib.load_manifest('BiotacCompliance')
+import rospy
+import time
+import sys
+from hubomsg.msg import *
+from biotac_sensors.msg import *
+
+RSR_UPPER = -1.3
+RSR_LOWER = .1
+REP_UPPER = -1.57
+REP_LOWER = 0
+
+INCREMENT = .05 # This number is untested
+ID_NUM = 14
+'''
+This is a demo class. It basically when created will handle
+the demo. Every time it gets a biotac_pub message it goes through
+the call back. 
+'''
+class demo:
+
+    def __init__(self):
+        self.MAX = 1
+        rospy.init_node("BioTacDemo")
+        rospy.Subscriber("biotac_pub", BioTacHand, self.update)
+        self.pub = rospy.Publisher("Maestro/Control", MaestroCommand)
+
+        self.REP = 0
+        self.RSR = 0
+
+        self.count = 0
+        rospy.on_shutdown(self.exit)
+        rospy.spin()
+    
+    '''
+    The heart of the demo 
+    '''
+    def update(self, handData):
+        data = handData.bt_data
+        electrodes = data[0].electrode_data
+        cd
+        self.pub.publish("RSP REP", "position position", str(self.RSP) + " " + str(self.REP), "", ID_NUM)
+
+    # Bend the elbow pitch
+    def moveLeft(self):
+        temp = self.REP - INCREMENT
+        if(temp > REP_UPPER):
+            self.REP = temp
+
+    # Straighten the elbow pitch
+    def moveRight(self):
+        temp = self.REP + INCREMENT
+        if(temp < REP_LOWER):
+            self.REP = temp
+
+    # Move the shoulder roll out
+    def moveUp(self):
+        temp = self.RSP - INCREMENT
+        if(temp > RSP_UPPER):
+            self.RSP = temp
+
+    # Move the shoulder roll in
+    def moveDown(self):
+        temp = self.RSP + INCREMENT
+        if(temp < REP_LOWER):
+            self.RSP = temp
+
+    def exit(self):
+        self.pub.publish("RSR REP", "position position", "0 0", "", ID_NUM)
+        
+
+
+    
+if __name__ == '__main__':
+    print "Starting the biotac elbow demo"
+    demo = demo()
+    
